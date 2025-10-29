c697805127d87fc323fd35492d3a0814b70eb0cf
mahabharathindipodcast-droid <mahabharathindipodcast@gmail.com>
2025-10-29 11:12:28 -0400

    Update ts.java

--- ts.java
+++ ts.java
@@ -5,15 +5,6 @@ from models.pubsub.pubsub_commit_event import PubSubCommitEvent
 
 class Commit(BaseModel):
     """Model for individual commit data."""
-    
-    id: str = Field(..., description="Commit SHA")
-    message: str = Field(..., description="Commit message")
-    author_name: str = Field(..., description="Author name")
-    author_email: str = Field(..., description="Author email")
-    author_username: str = Field(..., description="Author username")
-    files_added: List[str] = Field(..., description="List of files added")
-    files_removed: List[str] = Field(..., description="List of files removed")
-    files_modified: List[str] = Field(..., description="List of files modified")
     timestamp: str = Field(..., description="Commit timestamp")
     url: str = Field(..., description="API URL for the commit")
     html_url: str = Field(..., description="Web URL for the commit")
